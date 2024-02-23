82078ef7b18a9bd9ea9048417830fd41e3e47803
David Aryasetia <david.aryasetia@gmail.com>
2024-02-23 12:59:45 +0700

    computer vision

---
+++ test.dart
@@ -0,0 +1,12 @@
+void main() {
+  for (int i = 0; i < 5; i++) {
+    var stars = '';
+    for (int j = (5 - i); j > 1; j--) {
+      stars += ' ';
+    }
+    for (int j = 0; j <= i; j++) {
+      stars += '* ';
+    }
+    print(stars);
+  }
+}
